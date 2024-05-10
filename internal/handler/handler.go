450feb2d378c289aba2bb5d347219e9b7ff5cf8a
matvey <XapTMaH19@yandex.ru>
2024-05-10 11:24:05 +0300

    lesson3-4

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -1,8 +1,16 @@
 package handlers
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/XapTMaH19/todo-app/internal/service"
+	"github.com/gin-gonic/gin"
+)
 
 type Handler struct {
+	services *service.Service
+}
+
+func NewHandler(services *service.Service) *Handler {
+	return &Handler{}
 }
 
 func (h Handler) InitRoutes() *gin.Engine {
